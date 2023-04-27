7e24db220f38039de4efacbfc4fb74a184156842
Valentin <valentinwissler42@outlook.com>
2023-04-27 20:33:33 +0100

    all tests pass

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 import password_cracker
 from unittest import main
 
-cracked_password1 = password_cracker.crack_sha1_hash(
-    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
-print(cracked_password1)
+# cracked_password1 = password_cracker.crack_sha1_hash(
+#     "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
+# print(cracked_password1)
 
-cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
-print(cracked_password2)
+# cracked_password2 = password_cracker.crack_sha1_hash(
+#     "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
+# print(cracked_password2)
+
+# cracked_password3 = password_cracker.supermancracker(
+#     "53d8b3dc9d39f0184144674e310185e41a87ffd5")
+# print(cracked_password3)
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,50 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+hash = hashlib.sha1("password".encode()).hexdigest()
+
+def crack_sha1_hash(hash, use_salts=False):
+  fhandlePW = open("top-10000-passwords.txt", "r")
+  fhandleSalt = open("known-salts.txt", "r") if use_salts else None
+
+  for line in fhandlePW:
+    if use_salts:
+      # reset the file pointer to the beginning of the file
+      fhandleSalt.seek(0)
+      for salt in fhandleSalt:
+        h1 = hashlib.sha1((salt.strip() + line.strip()).encode("UTF-8")).hexdigest()
+        h2 = hashlib.sha1((line.strip() + salt.strip()).encode("UTF-8")).hexdigest()
+        if h1 == hash or h2 == hash:
+          fhandlePW.close()
+          fhandleSalt.close()
+          return line.strip()
+    else:
+      if hashlib.sha1((line.strip()).encode("UTF-8")).hexdigest() == hash:
+        fhandlePW.close()
+        return line.strip()
+  try: 
+    fhandlePW.close()
+    fhandleSalt.close()
+  except:
+    pass
+  return "PASSWORD NOT IN DATABASE"
+
+# def supermancracker(hash):
+  fhandleSalt = open("known-salts.txt", "r")
+  fwrite = open("superman.txt", "w")
+  for salt in fhandleSalt:
+    str1 = salt.strip() + "superman"
+    str2 = "superman" + salt.strip()
+    h1 = hashlib.sha1((salt.strip() + "superman").encode("UTF-8")).hexdigest()
+    h2 = hashlib.sha1(("superman" + salt.strip()).encode("UTF-8")).hexdigest()
+    fwrite.write(f"{str1} {h1}\n")
+    fwrite.write(f"{hash}\n")
+    fwrite.write(f"{str2} {h2}\n")
+    if h1 == hash or h2 == hash:
+      fwrite.write(f"FOUND: {salt.strip()}")
+      fwrite.close()
+      fhandleSalt.close()
+      return "superman"
+  fwrite.close()
+  fhandleSalt.close()
+  return "PASSWORD NOT IN DATABASE"
+        
